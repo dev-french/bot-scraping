dbd2603146a36a5df3bc2d1d817e9abb70812bc5
dev_french <dev_french@proton.me>
2022-10-29 21:12:11 +0200

    test target query and add file.json

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
     //  Parsing : Il va parser le code HTML à l’aide du sélecteur CSS ou du XPath pour extraire ce qui m'intéresse
     //  Stockage : Il va stocker le tout dans un fichier 
 
-
 function scraping($url) {    
 
-    $url = 'https://www.welcometothejungle.com/fr/jobs'; 
-
+    $url = 'https://www.welcometothejungle.com/fr/jobs?groupBy=job&sortBy=mostRelevant&query=laravel&aroundQuery=Paris%2C+France&refinementList%5Boffices.district%5D%5B0%5D=Paris&refinementList%5Boffices.state%5D%5B0%5D=Ile-de-France&refinementList%5Boffices.country_code%5D%5B0%5D=FR&page=1'; 
 
     // passage du contenu du fichier à une variable 
     $html = file_get_contents($url); //  cette function lis le fichier entier dans une chaîne de caractères, elle prend comme paramètre l'url
@@ -29,8 +27,8 @@ function scraping($url) {
 
         $site_path = new DOMXPath($site); // récupération du chemin du DOM (permet de faire les requetes)
 
-        $query = '/html/body//a[2]/@href'; // je définis ma cible dans le DOM
-
+        $query = '/html/body//header'; // je définis ma cible dans le DOM
+        // /html/body/div[1]/div/div/div/div/div/main/div[1]/div/div/div[2]/ol/li[1]/article/div[2]/header/div/a/h3
         // je recupere la requete
         $result = $site_path->query($query);
 
@@ -46,13 +44,13 @@ function scraping($url) {
             }
         }
 
-        // var_dump($scraped_data);
-        // die();
+        var_dump($scraped_data);
+        die();
 
-        
-    // création d'un fichier scraping.html pour stocker le contenu crawlé par le bot
-    file_put_contents('scraping.html',"\n\n".$scraped_data."\n\n",FILE_APPEND);  // FILE_APPEND permet d'ajouter du contenu au fichier existant
+    // création d'un fichier JSON pour stocker le contenu crawlé par le bot
+    file_put_contents('file.json', json_encode($scraped_data));
 
+    // file_put_contents('scraping.html',"\n\n".$scraped_data."\n\n",FILE_APPEND);  // FILE_APPEND permet d'ajouter du contenu au fichier existant
 
     }
     
