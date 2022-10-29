15cbd9eeeeb9eba975fc2995089bbb1cb647f2c4
dev_french <dev_french@proton.me>
2022-10-29 18:05:50 +0200

    difference scraping/crawling on README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Crawler 
+# Bot de web scraping
+
+## Différence entre scraping & crawling
+
+__Web crawling :__ alias l’indexation, est utilisé pour indexer l’information des pages web en utilisant des robots, aussi connus sous le nom de crawler. Les crawler web sont essentiellement utilisés par les principaux moteurs de recherche comme Google, Bing, Yahoo. En d’autre termes Google et Bing sont les principaux web crawler. Ici, nous récupérons une information générique, alors que le scraping récupère une information spécifique.
+
+__Web scraping :__ alias extraction de donnée du web, est un moyen automatisé d’extraire de l’information/du contenu en utilisant des robots (alias scraper). Ici, l’information peut être utilisée pour répliquer des sites internets ou pour de l’analyse de donnée.
+
+[Information, dans ce contexte, signifie toutes les variétés de contenu incluant image, texte, informations sensibles comme des contacts, prix…]
--- index.php
+++ index.php
@@ -1,30 +1,61 @@
 <?php
 
-// Voici comment va fonctionner notre bot de web scraping :
+// Voici comment va fonctionner mon bot de web scraping (extraction de données) :
 
-    //  Fetching : Il va se connecter à la page d’accueil du site et en télécharger le code HTML 
-    //  Parsing : Il va parser le code HTML à l’aide du sélecteur CSS ou du XPath pour extraire les liens vers chaque article
-    //  Stockage : Il va stocker les titres et les URL des offres d'emploi dans un fichier CSV
+    //  Fetching : Il va se connecter à la page du site et en télécharger le bout de code HTML choisis
+    //  Parsing : Il va parser le code HTML à l’aide du sélecteur CSS ou du XPath pour extraire ce qui m'intéresse
+    //  Stockage : Il va stocker le tout dans un fichier 
 
 
 function scraping($url) {    
 
-    $url = 'https://www.welcometothejungle.com/fr/jobs';
+    $url = 'https://www.welcometothejungle.com/fr/jobs'; 
 
 
     // passage du contenu du fichier à une variable 
     $html = file_get_contents($url); //  cette function lis le fichier entier dans une chaîne de caractères, elle prend comme paramètre l'url
 
-    // extraction des offres d'emplois
-    preg_match_all('~<a.*?href="(.*?)".*?>~',$html);  // extraction du contenu de la page
+     // stockage du DOM
+     $site = new DOMDocument();
 
-    // var_dump($matches);
-    // die();
+     // permet de supprimer les erreurs potentiel avec le DOM
+     libxml_use_internal_errors(TRUE);
 
-    // création d'un fichier html pour stocker le contenu crawlé
-    file_put_contents('results.html',"\n\n".$html."\n\n",FILE_APPEND);  
-  
+    if (!empty($html)) { // si le contenue de ma variable $html n'est pas vide alors je rentre dans cette condition
 
+        $site->loadHTML($html); //  Charge le HTML à partir d'une chaîne  (https://www.php.net/manual/en/domdocument.loadhtml.php)
+
+        libxml_clear_errors(); // supression des erreurs dans le DOM 
+
+        $site_path = new DOMXPath($site); // récupération du chemin du DOM (permet de faire les requetes)
+
+        $query = '/html/body//a[2]/@href'; // je définis ma cible dans le DOM
+
+        // je recupere la requete
+        $result = $site_path->query($query);
+
+        // var_dump($result);
+        // die();
+
+        $scraped_data = [];
+
+        // si la longeur du tableau est supérieur à zéro alors je rentre dans la condition
+        if ($result->length >0) {
+            foreach ($result as $item) {
+                $scraped_data[] = $item->nodeValue;
+            }
+        }
+
+        // var_dump($scraped_data);
+        // die();
+
+        
+    // création d'un fichier scraping.html pour stocker le contenu crawlé par le bot
+    file_put_contents('scraping.html',"\n\n".$scraped_data."\n\n",FILE_APPEND);  // FILE_APPEND permet d'ajouter du contenu au fichier existant
+
+
+    }
+    
 }
 
 scraping($url);
